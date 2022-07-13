ac5ab0d81a0eb84b98dfef5dc733d43d1ab8d287
kanazawanagisa <108912273+kanazawanagisa@users.noreply.github.com>
2022-07-13 12:45:13 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -25,7 +25,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Wecome!' + storedName;
+  myHeading.textContent = 'storedName+'さんようこそ';
 }
 myButton.onclick = function() {
   setUserName();
